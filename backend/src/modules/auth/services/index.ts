2749ea7751c89e8bbdc183981e640dc8605cd3c6
Diego Vite Hernández <undecode0@hotmail.com>
2025-10-17 17:23:43 -0600

    feat(auth): implementar la funcionalidad getMe para recuperar los datos del usuario.

--- backend/src/modules/auth/services/index.ts
+++ backend/src/modules/auth/services/index.ts
@@ -1,9 +1,11 @@
 import { loginService } from "./login.service";
 import { logoutService } from "./logout.service";
 import { refreshTokenService } from './refreshToken.service';
+import { getMeService } from './getMe.service';
 
 export {
   loginService,
   logoutService,
-  refreshTokenService
+  refreshTokenService,
+  getMeService
 }
