1df8ed5914201f70a0fe6e8065bc746c2a63fd8a
Diego Vite Hernández <undecode0@hotmail.com>
2025-10-17 16:42:18 -0600

    feat(core): añadir middleware CORS para gestionar los orígenes aceptados

---
+++ backend/src/shared/middlewares/cors.ts
@@ -0,0 +1,19 @@
+import cors from "cors";
+
+const ACCPETED_ORIGINS = ["http://localhost:5173"];
+
+export const corsMiddleware = ({ acceptedOrigins = ACCPETED_ORIGINS } = {}) =>
+  cors({
+    origin: (origin, cb) => {
+      if (acceptedOrigins.includes(origin!)) {
+        return cb(null, true);
+      }
+
+      if (!origin) {
+        return cb(null, true);
+      }
+
+      return cb(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  });
