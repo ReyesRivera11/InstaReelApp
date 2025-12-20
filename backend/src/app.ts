d36927c95d6ae56b08dffcdaaaf3eb166ca84a67
RAULCRESCENCIO <116508614+RAULCRESCENCIO@users.noreply.github.com>
2025-12-20 02:25:58 -0600

    back v2
    back terminado para sambox

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import path from "path";
 
 import { PORT } from './shared/config/env';
 import router from './shared/routes/index';
@@ -8,9 +9,13 @@ import { errorHandlerMiddleware } from './shared/middlewares/errorHandler';
 import { corsMiddleware } from './shared/middlewares/cors';
 import { HttpCode } from './shared/enums/HttpCode';
 
+import cron from "node-cron";
+import { processTikTokReels } from "./shared/jobs/tiktok-publisher.job";
+
 const app = express();
 
 app.disable('x-powered-by');
+app.set("trust proxy", 1);
 
 app.use(corsMiddleware());
 app.use(express.json());
@@ -19,6 +24,19 @@ app.use(morgan('dev'));
 
 app.get('/healthcheck', (_req: Request, res: Response) => {
   res.sendStatus(HttpCode.OK);
+});
+
+app.use(
+   "/uploads",
+   express.static(path.join(process.cwd(), "uploads"))
+);
+
+cron.schedule("* * * * *", async () => {
+   try {
+      await processTikTokReels();
+   } catch (error) {
+      console.error("❌ Error running TikTok cron:", error);
+   }
 });
 
 // Routes
